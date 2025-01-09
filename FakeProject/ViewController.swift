639b0771c1d8b71ce34b6cdcc6984de00f34d33f
Kushagra Kulshrestha <140870916+Ricky30feb@users.noreply.github.com>
2025-01-09 15:37:17 +0530

    Initial Commit

---
+++ FakeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeProject
+//
+//  Created by Batch - 2 on 09/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
